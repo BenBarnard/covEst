f35a62a221a89e248f90e5142796f677bd2e6fc0
Gabriel Odom <gjo15@miami.edu>
2017-08-25 13:01:16 -0400

    Added lazy_dots() call within the Haff_Shrinkage() function. Wrote the help file for this function, and included a link to the original Haff (1979) paper.

--- R/Haff_shrinkage.R
+++ R/Haff_shrinkage.R
@@ -1,9 +1,17 @@
 #' Haff Shrinkage Precision Estimator
 #'
 #' @param x data matrix
-#' @param ... other options
+#' @param ... other options (currently unused)
 #'
 #' @return Haff shrinkage precision estimator
+#'
+#' @details Given a matrix of observations, this function will calculate the
+#'    Haff Shrinkage Estimator of the sample precision matrix, as discussed in
+#'    \href{https://projecteuclid.org/euclid.aos/1176344845}{Haff (1979)}.
+#'    Because this estimator relies on the existence of the sample precision
+#'    matrix (and relatedly, a strictly positive covariance determinant), this
+#'    estimator is ill-suited for high-dimensional cases (\eqn{N < p}).
+#'
 #' @export
 #'
 #' @importFrom lazyeval lazy_dots
@@ -11,6 +19,7 @@
 #'
 #' @examples Haff_shrinkage(as.matrix(iris[-5]))
 Haff_shrinkage <- function(x, ...){
+  dots <- lazy_dots(...)
   cov <- stats::cov(x)
   invCov <- solve(cov)
   n <- nrow(x)
