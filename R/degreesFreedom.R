36de3491aaef7b1f88a5bc90361af49988af4dfd
Gabriel Odom <gjo15@miami.edu>
2017-08-25 10:30:43 -0400

    Updated documentation for the degreesFreedom() function.

--- R/degreesFreedom.R
+++ R/degreesFreedom.R
@@ -1,12 +1,23 @@
-#' Title
+#' Degrees of Freedom of a Covariance Matrix
 #'
-#' @param x covariance class matrix
+#' @param x a matrix with class \code{covariance}
 #'
-#' @return Degrees of freedom of the covariance matrix
+#' @return The degrees of freedom of the covariance matrix, as a \code{numeric}
+#'    object
+#'
+#' @details This function uses the \code{f_eval} function from the \code{lazyeval}
+#'    package to return the degrees of freedom stored within the \code{df}
+#'    attribute of a \code{matrix} object having the additional class
+#'    \code{covariance}. This evaluation is necessary because the \code{df}
+#'    attribute of a matrix with class \code{covariance} is stored as a
+#'    \code{formula} object.
+#'
 #' @export
 #' @importFrom lazyeval f_eval
 #'
-#' @examples degreesFreedom(cov(iris[,1:4]))
+#' @examples
+#' degreesFreedom(cov(iris[,1:4]))
+#' lapply(cov(iris, group = "Species"), degreesFreedom)
 degreesFreedom <- function(x){
   f_eval(attributes(x)$df)
 }
